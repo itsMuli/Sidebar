0c57d812176d3cce9b69705e1bd55b59bb9c8127
Rose Muli <rosemwendwa163@gmail.com>
2022-09-08 15:52:20 +0300

    navbarchanges

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -21,19 +21,38 @@ const NavButton = ({ title, customFunc, icon,
         <span style={{ background: dotColor}}
           className="absolute inline-flex
           rounded-full h-2 w-2 right-2 top-2"
-          >
+        />
             {icon}
-        </span>
-
-
       </button>
 
     </TooltipComponent>
   )
 
 const Navbar = () => {
-  const { activeMenu, setActiveMenu,handleClick } =
+  const { activeMenu, setActiveMenu, handleClick, 
+  isClicked, setIsClicked, screenSize, setScreenSize } =
   useStateContext();
+
+  useEffect(() => {
+    const handleResize = () =>setScreenSize
+    (window.innerWidth);
+
+    window.addEventListener('resize',
+    handleResize);
+
+    handleResize();
+
+    return () => window.removeEventListener
+    ('resize', handleResize)
+  }, []);
+
+  useEffect(() => {
+    if(screenSize <=900) {
+      setActiveMenu(false);
+    } else {
+      setActiveMenu(true);
+    }
+  }, [screenSize]);
 
   return (
     <div className="flex justify-between p-2
@@ -67,7 +86,32 @@ const Navbar = () => {
         color="blue"
         icon={<RiNotification3Line />}
       />
-      
+      <TooltipComponent
+        content="Profile"
+        position="BottomCenter"
+      >
+        <div className="flex items-center
+        gap-2 cursor-pointer p-1
+        hover:bg-light-gray rounded-lg"
+        onClick={() => handleClick
+        ('userProfile')}>
+          <img
+            className="rounded-full w-8 h-8"
+            src={avatar}
+          />
+          <p>
+            <span className="text-gray-400 text-14">Hi, </span> { '' }
+            <span className="text-gray-400 font-bold ml-1 text-14">Michael</span>
+          </p>
+          <MdKeyboardArrowDown
+          className="text-gray-400 text-14" />
+        </div>
+      </TooltipComponent>
+
+      {isClicked.cart && <Cart />}
+      {isClicked.chat && <Chat />}
+      {isClicked.notification && <Notification />}
+      {isClicked.userProfile && <UserProfile />}
       </div>
     </div>
   )
--- src/components/ThemeSettings.jsx
+++ src/components/ThemeSettings.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const ThemeSettings = () => {
   return (
     <div>
-      
+      ThemeSettings
     </div>
   )
 }
